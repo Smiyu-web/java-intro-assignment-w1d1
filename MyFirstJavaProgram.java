aab1f3e644557ba7472af79366be6b58b0c12249
Miu <miu@MacBook-Pro.hitronhub.home>
2020-10-05 10:39:28 -0700

    first commit

---
+++ MyFirstJavaProgram.java
@@ -0,0 +1,15 @@
+public class MyFirstJavaProgram {
+
+    public static void main(String[] args) {
+
+        short smallNum = 200;
+
+        System.out.println(smallNum);
+
+        smallNum = 500;
+
+        System.out.println(smallNum);
+
+
+    }
+}
